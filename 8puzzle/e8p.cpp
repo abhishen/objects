6b3c1cf57718ac7c8afe7146acc58db6fd06ee0d
abhishen <abhisheknaik.23@gmail.com>
2017-11-14 01:01:56 -0800

    Added puzzle logic

--- 8puzzle/e8p.cpp
+++ 8puzzle/e8p.cpp
@@ -12,6 +12,86 @@ All includes here
 -----------------------------------------------------------------*/
 #include "e8p.h"
 
+/*
+node constructor
+*/
+node::node(const int array[N][N]) {
+	//Node stores the configuration
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			_config[i][j] = array[i][j];
+		}
+	}
+}
+
+/*
+e8p constructor
+*/
+
+e8p::e8p(const int s[N][N], const int f[N][N]):_solution(""),_game(s) {
+	//e8p creates board
+	//Board stores the unordered_map of nodes and queue of nodes
+	//Board also stores the starting config in node _origin
+}
+
+/*
+e8p destructor
+*/
+e8p::~e8p() {
+	delete _game;
+}
+
+int e8p::get_num_moves() const {
+	return _moves;
+}
+
+string e8p::get_solution() const {
+	return _solution;
+}
+
+/*
+board constructor
+*/
+board::board(const int s[N][N]):_solution(""),_origin(s) {
+}
+
+bool board::_isValid(char c) {
+	int row = -1;
+	int col = -1;
+	for (int i = 0; i < N; ++i) {
+		for (int j = 0; j < N; ++j) {
+			int v = _origin [i][j];
+			if (v == 0) {
+				row = i;
+				col = j;
+				break;
+			}
+		}
+	}
+
+	if (c == 'U') {
+		int newPosition = row - 1;
+		return (newPosition >= 0);
+	}
+	else if (c == 'L') {
+		int newPosition = col - 1;
+		return (newPosition >= 0);
+	}
+	else if (c == 'D') {
+		int newPosition = row + 1;
+		return (newPosition < N);
+	}
+	else {
+		int newPosition = col + 1;
+		return (newPosition < N);
+	}
+}
+
+node& board::configure(char c) {
+	if (_isValid(c)) {
+		node temp;
+	}
+}
 
 //EOF
 
--- 8puzzle/e8p.h
+++ 8puzzle/e8p.h
@@ -23,7 +23,22 @@ Declaration of board
 -----------------------------------------------------------------*/
 class board{
 public:
-  
+	static const int N = 2;
+	board(const int arr[N][N]);
+	
+	const node& configure(char);
+	bool insert_map(node);
+	void push(node);
+	node pop();
+	bool empty();
+
+private:
+	char _dirs [4] = { 'U', 'L', 'D', 'R' };
+	bool _isValid(char c);
+	node _origin;
+	unordered_map <node,int> _map;
+	queue <node> _q;
+	string _solution;
 };
 
 /*----------------------------------------------------------------
@@ -31,7 +46,10 @@ Node definition
 -----------------------------------------------------------------*/
 class node {
 public:
-  
+	static const int N = 3;
+	node(const int array[N][N]);
+private:
+	vector<vector<int>> _config;
 };
 
 /*----------------------------------------------------------------
@@ -48,7 +66,8 @@ public:
   string get_solution() const ;
 
 private:
- 
+	board _game;
+	string _solution;
 };
 
 
