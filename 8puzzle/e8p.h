dd24af772eca5185fd449f09d45f650f919527db
abhishen <abhisheknaik.23@gmail.com>
2017-11-14 17:37:14 -0800

    Removed changes

--- 8puzzle/e8p.h
+++ 8puzzle/e8p.h
@@ -23,22 +23,7 @@ Declaration of board
 -----------------------------------------------------------------*/
 class board{
 public:
-	static const int N = 2;
-	board(const int arr[N][N]);
-	
-	const node& configure(char);
-	bool insert_map(node);
-	void push(node);
-	node pop();
-	bool empty();
-
-private:
-	char _dirs [4] = { 'U', 'L', 'D', 'R' };
-	bool _isValid(char c);
-	node _origin;
-	unordered_map <node,int> _map;
-	queue <node> _q;
-	string _solution;
+  
 };
 
 /*----------------------------------------------------------------
@@ -46,11 +31,7 @@ Node definition
 -----------------------------------------------------------------*/
 class node {
 public:
-	static const int N = 3;
-	node(const int array[N][N]);
-	node&(const node&);
-private:
-	vector<vector<int>> _config;
+  
 };
 
 /*----------------------------------------------------------------
@@ -67,8 +48,7 @@ public:
   string get_solution() const ;
 
 private:
-	board _game;
-	string _solution;
+ 
 };
 
 
